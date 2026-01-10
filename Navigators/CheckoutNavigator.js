f46f7f53472d6af2b3448fb53f13e44de48123f0
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-10 10:47:57 +0800

    cart

--- Navigators/CheckoutNavigator.js
+++ Navigators/CheckoutNavigator.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 // Screens
-// import Checkout from '../Screens/Cart/Checkout/Checkout'
-// import Payment from '../Screens/Cart/Checkout/Payment'
-// import Confirm from '../Screens/Cart/Checkout/Confirm';
+import Checkout from '../Screens/Checkout/Checkout';
+import Payment from '../Screens/Checkout/Payment';
+import Confirm from '../Screens/Checkout/Confirm';
 
 const Tab = createMaterialTopTabNavigator();
 
 function MyTabs() {
     return (
         <Tab.Navigator>
-            {/* <Tab.Screen name="Shipping" component={Checkout} />
+            <Tab.Screen name="Shipping" component={Checkout} />
             <Tab.Screen name="Payment" component={Payment} />
-            <Tab.Screen name="Confirm" component={Confirm} /> */}
+            <Tab.Screen name="Confirm" component={Confirm} />
         </Tab.Navigator>
     );
 }
